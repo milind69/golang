aaa5faae4403ff9afb2bf804f06f2f0ffa07263c
Milind Kulkarni <36119958+milind69@users.noreply.github.com>
2022-06-06 09:54:43 -0500

    added itoa

--- type_conversion.go
+++ type_conversion.go
@@ -13,6 +13,10 @@ func main() {
 		panic(err)
 	}
 
+	x := strconv.Itoa(i)
+
 	fmt.Printf("The type of s is %T  and values is %v\n", s, s)
 	fmt.Printf("The type of i is %T  and values is %v\n", i, i)
+	fmt.Printf("The type of x is %T  and values is %v\n", x, x)
+
 }
